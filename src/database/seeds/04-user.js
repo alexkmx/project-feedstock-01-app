28c955aca897898e9ba45df24c165e7cc134b94b
Alejandro <alexk_mx@hotmail.com>
2017-12-19 16:38:50 -0600

    create seed 04-user.js

---
+++ src/database/seeds/04-user.js
@@ -0,0 +1,15 @@
+const dataRows = [
+  {
+    email: 'usuario@feedstock.com',
+    password: 12345
+  }
+];
+
+exports.seed = function(knex, Promise) {
+  return knex('user')
+    .del()
+    .then(() => {
+      return knex('user')
+        .insert(dataRows);
+    });
+}
